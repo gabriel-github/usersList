261c219d9c77242380d2f901c48b43f486ba5f9e
gabriel-github <gabrielferrsantos201@gmail.com>
2020-12-27 07:15:05 -0300

    arrumando erros

--- src/App.js
+++ src/App.js
@@ -1,66 +1,10 @@
-import React, { useState } from "react";
-import Comentarios from "./components/Comentarios";
-import { Container } from "./components/AppStyle/style";
+import React from "react";
+import Usuarios from "./components/Usuarios"
+
 
 function App() {
-  const [list, setList] = useState([]);
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-
-  const removeItem = (item) => {
-    const newList = list.filter((i) => i !== item);
-    setList(newList);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setList([...list, { first_name: name, email: email }]);
-
-    setName('')
-    setEmail('')
-  };
-
   return (
-    <Container>
-      <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="name"
-          required
-          autoFocus
-        />
-
-        <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="email"
-          required
-          autoFocus
-        />
-
-        <div>
-        <button type="submit">submeter</button>
-        </div>
-      </form>
-
-      <div>
-        {list.map((item, index) => (
-          <Comentarios
-            key={index}
-            id={index}
-            name={item.first_name}
-            email={item.email}
-            date={item.date}
-            remove={removeItem.bind(this, item)}
-          />
-        ))}
-      </div>
-    </Container>
+    <Usuarios/>
   );
 }
 
